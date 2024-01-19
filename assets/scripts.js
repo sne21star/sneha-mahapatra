33fe6ee63284e8ca08b15fa2dd888d79f533adcf
snehamahapatra <mahapat0@purdue.edu>
2024-01-18 22:41:25 -0500

    Ready to be deployed

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,90 +2,99 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-class Random {
-    
-    static generateBinary() {
-        var random = Math.random();
-        if (random > 0.5) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-    
-    static generate(min, max) {
-        return Math.floor((Math.random() * max) + min);
-    }
-    
+function expandImage(imgs, expandImg, imgtext) {
+  var expandImg = document.getElementById(expandImg);
+  var imgText = document.getElementById(imgtext);
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
 }
 
-class Binary {
-    constructor() {
-        this.value = Random.generateBinary();
-    }
+// var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+// class Random {
     
-    animate(ts, leftOffset, topOffset) {
-        var div = document.createElement("div");
-        $(div).css("font-size", ts + "px");
-        $(div).css("top", topOffset * (ts / 2));
-        $(div).css("left", leftOffset + "px");
-        $(div).text(this.value);
-        $(div).addClass("binary");
-        $(div).hide();
-        $("body").append(div);
-        $(div).show().addClass("animated fadeIn").on(animationEnd, this.fadeInEnd);
-        return $(div).offset().top;
-    }
+//     static generateBinary() {
+//         var random = Math.random();
+//         if (random > 0.5) {
+//             return 1;
+//         } else {
+//             return 0;
+//         }
+//     }
+    
+//     static generate(min, max) {
+//         return Math.floor((Math.random() * max) + min);
+//     }
+    
+// }
+
+// class Binary {
+//     constructor() {
+//         this.value = Random.generateBinary();
+//     }
+    
+//     animate(ts, leftOffset, topOffset) {
+//         var div = document.createElement("div");
+//         $(div).css("font-size", ts + "px");
+//         $(div).css("top", topOffset * (ts / 2));
+//         $(div).css("left", leftOffset + "px");
+//         $(div).text(this.value);
+//         $(div).addClass("binary");
+//         $(div).hide();
+//         $("body").append(div);
+//         $(div).show().addClass("animated fadeIn").on(animationEnd, this.fadeInEnd);
+//         return $(div).offset().top;
+//     }
    
-    fadeInEnd(event) {
-        var $binary = $(event.currentTarget);
-        $binary.removeClass("animated fadeIn");
-        $binary.addClass("animated fadeOut").on(animationEnd, function() {
-            $binary.remove();
-        });
-    }
-}
+//     fadeInEnd(event) {
+//         var $binary = $(event.currentTarget);
+//         $binary.removeClass("animated fadeIn");
+//         $binary.addClass("animated fadeOut").on(animationEnd, function() {
+//             $binary.remove();
+//         });
+//     }
+// }
 
-class BinaryLine {
-    constructor(lO, tS, dS) {
-        this.leftOffset = lO;
-        this.textSize = tS;
-        this.documentSize = dS;
-    }
+// class BinaryLine {
+//     constructor(lO, tS, dS) {
+//         this.leftOffset = lO;
+//         this.textSize = tS;
+//         this.documentSize = dS;
+//     }
     
-    generate() {
-        var iterator = 1;
-        var size = this.length;
-        var fontSize = this.textSize;
-        var documentSize = this.documentSize;
-        var currentOffset = 0;
-        var leftOffset = this.leftOffset;
-        var interval = setInterval(function() {
-            if (currentOffset < documentSize) {
-                var binary = new Binary();
-                currentOffset = binary.animate(fontSize, leftOffset, iterator);
-                iterator++;
-            } else {
-                clearInterval(interval);
-            }
-        }, 80);
-    }
-}
+//     generate() {
+//         var iterator = 1;
+//         var size = this.length;
+//         var fontSize = this.textSize;
+//         var documentSize = this.documentSize;
+//         var currentOffset = 0;
+//         var leftOffset = this.leftOffset;
+//         var interval = setInterval(function() {
+//             if (currentOffset < documentSize) {
+//                 var binary = new Binary();
+//                 currentOffset = binary.animate(fontSize, leftOffset, iterator);
+//                 iterator++;
+//             } else {
+//                 clearInterval(interval);
+//             }
+//         }, 80);
+//     }
+// }
 
-class BinaryAnimation {
-    constructor() {}
+// class BinaryAnimation {
+//     constructor() {}
     
-    start() {
-        setInterval(function() {
-            new BinaryLine(Random.generate(0, $(document).width()), Random.generate($(document).width() * 0.002, $(document).width() * 0.008), $(document).height()).generate();
-        }, 400);
+//     start() {
+//         setInterval(function() {
+//             new BinaryLine(Random.generate(0, $(document).width()), Random.generate($(document).width() * 0.002, $(document).width() * 0.008), $(document).height()).generate();
+//         }, 400);
         
-        setInterval(function() {
-            $(".binary").remove();
-        }, 30000)
-    }
-}
+//         setInterval(function() {
+//             $(".binary").remove();
+//         }, 30000)
+//     }
+// }
 
-new BinaryAnimation().start();
+// new BinaryAnimation().start();
